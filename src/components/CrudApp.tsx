d2ea478f45b6960798d29424b1da2768fca2c71d
Alexis <alexise857@gmail.com>
2022-03-01 16:56:12 -0500

    first commit

---
+++ src/components/CrudApp.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const initialDb = [
+    {
+        id: 1,
+        name: "Seiya",
+        constellation: "Pegaso",
+    },
+    {
+        id: 2,
+        name: "Shiryu",
+        constellation: "Dragón",
+    },
+    {
+        id: 3,
+        name: "Hyoga",
+        constellation: "Cisne",
+    },
+    {
+        id: 4,
+        name: "Shun",
+        constellation: "Andrómeda",
+    },
+    {
+        id: 5,
+        name: "Ikki",
+        constellation: "Fénix",
+    },
+];
+
+const CrudApp = () => {
+    return (
+        <div>
+            <h2>CRUD APP</h2>
+        </div>
+    );
+};
+
+export default CrudApp;
